3a9fe4ea3788c2f5a1b6ad63bd0703182e89b6ad
Jonas Keller <jonas.r.keller@gmail.com>
2024-11-04 13:05:16 +0100

    Use initializer list in Entity constructor

--- src/tinyECS/tiny_ecs.hpp
+++ src/tinyECS/tiny_ecs.hpp
@@ -10,11 +10,8 @@ class Entity
     unsigned int id;
     static unsigned int id_count; // starts from 1, entity 0 is the default initialization
 public:
-    Entity()
-    {
-        id = id_count++;
-        // Note, indices of already deleted entities aren't re-used in this simple implementation.
-    }
+    // Note, indices of already deleted entities aren't re-used in this simple implementation.
+    Entity() : id(id_count++) {}
     operator unsigned int() { return id; } // this enables automatic casting to int
 };
 
